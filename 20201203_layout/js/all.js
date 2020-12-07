06eae4f87f74e3447b71e05653fe2e5cc23bd7ab
Jacko <jacko1114@hotmail.com>
2020-12-07 14:21:13 +0800

    update : 1207

--- 20201203_layout/js/all.js
+++ 20201203_layout/js/all.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
         if (scroll_height > 60) {
             $("#gotop").addClass("active");
             $("nav").addClass("active");
-            $(".banner").css("padding-top", `${nav_height}px`)
+            $(".banner").css("padding-top", `${nav_height}px`);
         } else {
             $("#gotop").removeClass("active");
             $("nav")
                 .find("a")
                 .removeClass("active");
             $("nav").removeClass("active");
-            $(".banner").css("padding-top", "0")
+            $(".banner").css("padding-top", "0");
         }
     });
     $("#gotop").on("click", function (e) {
