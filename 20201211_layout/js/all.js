48f9abef716f879cbb6281f779d3c816c719c9a2
Jacko <jacko1114@hotmail.com>
2020-12-12 23:43:51 +0800

    upload homework : 1212

--- 20201211_layout/js/all.js
+++ 20201211_layout/js/all.js
@@ -1,8 +1,16 @@
-$(function(){
-    if($(".menu-btn").is(":visble")){
-        $(".header-menu").hide();
-        $(".menu-btn").on("click",function(){
-            $(".header-menu").toggle();
-        })
+let btn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".header-menu");
+
+window.addEventListener("resize",function(){
+    menu.classList.add("inactive");
+})
+
+btn.addEventListener("click",function(){
+    if(!menu.classList.contains("active")){
+        menu.classList.add("active");
+        menu.classList.remove("inactive");
+    }else{
+        menu.classList.remove("active");
+        menu.classList.add("inactive");
     }
-});
+})
