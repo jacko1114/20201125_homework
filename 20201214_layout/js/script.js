cfd1291e6c73cebd2967e56d55ccdb4212b10309
Jacko <jacko1114@hotmail.com>
2020-12-15 16:42:10 +0800

    update side menu : 1215

---
+++ 20201214_layout/js/script.js
@@ -0,0 +1,12 @@
+// let nav = document.querySelector("nav");
+
+// window.addEventListener("scroll",function(){
+//     if(nav.scrollHeight - nav.scrollTop === nav.clientHeight){
+//         nav.style.position = "fixed";
+//         nav.style.top = 0;
+//         nav.style.left = 0;
+//     }else{
+//         nav.style.position = "static";
+//     }
+
+// })
